e6ef9c4470b74603cf4243a094319e458d95c867
xijieyin <xijieyin@gmail.com>
2022-10-20 12:10:51 +0800

    chore: initial release

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,58 @@
-import { message } from './constants.js'
-
-export const main = () => {
-	console.log(message)
+import crypto, { pbkdf2 as deriveKey } from 'node:crypto'
+export function pbkdf2({ password, salt }: Pbkdf2Input): Promise<Buffer> {
+	return new Promise((resolve, reject) => {
+		deriveKey(password, salt, 1, 64, 'sha256', (error, key) => {
+			if (error) return reject(error)
+			resolve(key)
+		})
+	})
 }
 
-main()
+function generateRandom() {
+	return Buffer.from(crypto.randomBytes(16))
+}
+
+export async function aesEncrypt(data: string, password: string) {
+	const salt = generateRandom()
+	const iv = generateRandom()
+	const key = await pbkdf2({ password, salt })
+	const encryptTool = crypto.createCipheriv(
+		'aes-256-cbc',
+		key.subarray(0, 32),
+		iv,
+	)
+	let encryptedContent = encryptTool.update(data, 'utf8', 'base64')
+	encryptedContent += encryptTool.final('base64')
+	console.log(encryptedContent)
+
+	const packaged = Buffer.concat([
+		iv,
+		salt,
+		Buffer.from(encryptedContent, 'base64'),
+	])
+	return packaged.toString('base64')
+}
+
+export async function aesDecrypt(data: string, password: string) {
+	const unpackaged = Buffer.from(data, 'base64')
+	const iv = unpackaged.subarray(0, 16)
+	const salt = unpackaged.subarray(16, 32)
+	const input = unpackaged.subarray(32)
+	const key = await pbkdf2({ password, salt })
+	const decryptTool = crypto.createDecipheriv(
+		'aes-256-cbc',
+		key.subarray(0, 32),
+		iv,
+	)
+	decryptTool.update(
+		input.toString('base64'),
+		'base64',
+		'utf8',
+	)
+	return decryptTool.final('utf8')
+}
+
+type Pbkdf2Input = {
+	readonly password: string
+	readonly salt: Buffer
+}
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,6 +1,26 @@
 import { expect, test } from 'vitest'
-import { message } from '../src/constants.js'
+import { aesDecrypt, aesEncrypt, pbkdf2 } from '../src/index.js'
 
-test("message contains 'Hello'", () => {
-	expect(message).toContain('Hello')
+test('pbdf2', async () => {
+	const key = await pbkdf2({
+		password: '121212',
+		salt: Buffer.from('2Rj6a45U7sJFOTEAhE8N1w==', 'base64'),
+	})
+	expect(key.toString('base64')).toBe(
+		'LsyRUO6ILDO1DTVEa3m/ALEUHXjeMndZ1gnNNrzsSZelfYG1OyBKMoieb8B1mw7I4dUCskMxRtQNWEHQBC1GsA==',
+	)
+})
+
+test('encrypt data', async () => {
+	const data = 'test'
+	const encrypted = await aesEncrypt(data, '121212')
+	console.log(encrypted)
+	const decrypted = await aesDecrypt(encrypted, '121212')
+	expect(decrypted).toBe('test')
+})
+
+test('decrypt data from dart', async () => {
+	const data = 's2zivTgU8zdPkAYGYWNVUpshrCg5AhiCbb4O6DmbDy+DmHkMwVgRYQKi8+xPbDIn'
+	const decrypted = await aesDecrypt(data, '121212')
+	expect(decrypted).toBe('test')
 })
